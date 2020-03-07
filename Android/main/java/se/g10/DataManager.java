9789fddf32a33508e34d92f09c99b84b024f31f2
donlin <chendonglin98741236@126.com>
2020-03-07 11:17:40 +0800

    update android code

--- Android/main/java/se/g10/DataManager.java
+++ Android/main/java/se/g10/DataManager.java
@@ -42,30 +42,24 @@ public class DataManager extends Thread{
 
     public void run(){
         try {
-            URL url = new URL("http://192.168.199.224:2020/android/doctors");
-            //2. HttpURLConnection
+            URL url = new URL("http://0cdl.com:2020/android/doctors");
+
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             //3. POST
             conn.setRequestMethod("POST");
-            //4. Content-Type,这里是固定写法，发送内容的类型
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            //5. output，这里要记得开启输出流，将自己要添加的参数用这个输出流写进去，传给服务端，这是socket的基本结构
             conn.setDoOutput(true);
             OutputStream os = conn.getOutputStream();
             String param = "haha";
-            //一定要记得将自己的参数转换为字节，编码格式是utf-8
             os.write(param.getBytes("utf-8"));
             os.flush();
-            //6. is
             InputStream is = conn.getInputStream();
-            //7. 解析is，获取responseText
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             StringBuilder sb = new StringBuilder();
             String line = null;
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
             }
-            //获取响应文本
             String responseText = sb.toString();
             Log.d("test",responseText);
             String[] r=responseText.split(splitKey);
