a2442b3bdc6d6440bd7fbda8d4857962f611ff3b
CDTD-LLT <chendonglin@emails.bjut.edu.cn>
2020-04-09 08:00:00 +0800

    realize some pages

--- Web/healingpaws/config.py
+++ Web/healingpaws/config.py
@@ -13,5 +13,5 @@ class DatabaseSecretConfig(object):
     # dasdasdasdsdas
 	#asdasdasasdadsadsassadassdasdsadassadssdsdsasdasdsadsadssdads
 	#sdasdssadassdadssds
-    #asdasdsasdsds
+    #asdasdsasdsdssdsd
 	#sdasdsdasdas
