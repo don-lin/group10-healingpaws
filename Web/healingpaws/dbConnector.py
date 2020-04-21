4e19924646566a62ff0b754ed55cc646aa29783d
donlin <chendonglin98741236@126.com>
2020-04-21 09:06:06 +0800

    update code

---
+++ Web/healingpaws/dbConnector.py
@@ -0,0 +1,257 @@
+import  time
+from werkzeug.security import check_password_hash, generate_password_hash
+from healingpaws import db
+from healingpaws.models import User,Pets,Doctors,Appointments,Question,Reply
+
+
+def checkUserExist(username):
+    return not getUser(username) is None
+
+#检查这个用户是否是员工
+def checkIfEmployee(username):
+    user = getUser(username)
+    if user.user_level == 2:
+        return True
+    else:
+        return False
+
+def checkUserPassword(username,userPassword):
+    users=getAllUser()
+    for u in users:
+        if(username==u.username and check_password_hash(u.passwordHash,userPassword)):
+            return True
+    return False
+
+def addUser(username,password):
+    u1=User(username=username,passwordHash=generate_password_hash(password))
+    db.session.add(u1)
+    db.session.commit()
+
+def updateUserBirthday(username,birthday):
+    if not birthday:
+        return
+    User.query.filter_by(username=username).update({'dateOfBirth':birthday})
+    db.session.commit()
+def updateUserEmail(username,email):
+    if not email:
+        return
+    User.query.filter_by(username=username).update({'email':email})
+    db.session.commit()
+def updateUserGender(username,isMale):
+    User.query.filter_by(username=username).update({'isMale':isMale})
+    db.session.commit()
+def updateUserPassword(username,newPassword):
+    newHash=generate_password_hash(newPassword)
+    User.query.filter_by(username=username).update({'passwordHash':newHash})
+    db.session.commit()
+
+def deleteUser(username):
+    User.query.filter_by(username=username).delete()
+    db.session.commit()
+
+def getAllUser():
+    return User.query.all()
+
+def getUser(username):
+    users=User.query.filter_by(username=username).all()
+    for u in users:
+        if u.username==username:
+            return u
+    return None
+
+def getUserFromId(userid):
+    users=getAllUser()
+    for u in users:
+        if u.id==userid:
+            return u
+    return None
+
+def showUsers(users):
+    for u in users:
+        print(u)
+
+def getAllUserNameList():
+    users=getAllUser()
+    result=[]
+    for u in users:
+        result.append(u.username)
+    return result
+
+
+
+#这个函数更改为返回添加的这个pet的id
+def addPet(petname,ownername,petHealth,birthday):
+    userid=getUser(ownername).id
+    print('\033[44m')
+    print('user id is :'+str(userid))
+    print('\033[0m')
+    p1=Pets(petsname=petname,owner=userid,health=petHealth,birthDay=birthday)
+    db.session.add(p1)
+    db.session.commit()
+    return p1.id
+
+
+def updatePet(petid,petname,petHealth,birthday):
+    Pets.query.filter_by(id=petid).update({'petsname':petname,'health':petHealth,'birthDay':birthday})
+    db.session.commit()
+
+#employee专用
+def updatePet_e(petid,petHealth):
+    Pets.query.filter_by(id=petid).update({'health':petHealth})
+    db.session.commit()
+
+def deletePet(petid):
+    Pets.query.filter_by(id=petid).delete()
+    db.session.commit()
+
+def getAllPets():
+    return Pets.query.all()
+
+def getAllQuestions():
+    return Question.query.all()
+
+def getAllReplies():
+    return Reply.query.all()
+
+def getUserPets(username):
+    userid=getUser(username).id
+    return Pets.query.filter_by(owner=userid).all()
+
+
+def addDoctor(name,age,telphone,introduction):
+    d1=Doctors(doctorname=name,age=age,telphone=telphone,introduction=introduction)
+    db.session.add(d1)
+    db.session.commit()
+
+def updateDoctor(docid,name,age,telphone,introduction):
+    Doctors.query.filter_by(id=docid).update({'doctorname':name,'age':age,'telphone':telphone,'introduction':introduction})
+    db.session.commit()
+
+def getAllDoctors():
+    return Doctors.query.all()
+
+def deleteDoctor(docid):
+    Doctors.query.filter_by(id=docid).delete()
+    db.session.commit()
+
+
+def genNewComment(username,content):
+    result=username+"_;_"+getTime()+"_;_"+content+"_;_"
+    return result
+
+
+def filtMessage(username,messages):
+    result=[]
+    for m in messages:
+        if(m.get('username')==username):
+            result.append(m)
+    return result
+
+
+def getTime():
+    return time.strftime("%Y/%m/%d  %I:%M:%S")
+
+def addReply(user_id, content, q_id):
+    r = Reply(user=user_id, content=content, question_id=q_id)
+    db.session.add(r)
+    db.session.commit()
+
+
+def addAppointment(petid,doctorid,date,emergency,description):
+    a=Appointments(pet=petid,doctor=doctorid,date=date,emergency=emergency,description=description)
+    db.session.add(a)
+    db.session.commit()
+
+def updateAppointment(appointmentid,petid,doctorid,date):
+    Appointments.query.filter_by(id=appointmentid).update({'pet':petid,'doctor':doctorid,'date':date})
+    db.session.commit()
+
+#员工操作的更新
+def updateAppointment_e(appointmentid,doctorid,date,time_slot,status):
+    Appointments.query.filter_by(id=appointmentid).update({'doctor':doctorid,'date':date,'time_slot':time_slot,'status':status})
+    db.session.commit()
+
+#根据Pet找Appointment
+def getPetsAppointment(petid):
+    return Appointments.query.filter_by(pet=petid).all()
+
+def getAllAppointment():
+    return Appointments.query.all()
+
+def getUserAppointment(username):
+    pets=getUserPets(username)
+    result=[]
+    for p in pets:
+        result+=getPetsAppointment(p.id)
+    return result
+
+def get_question_by_id(id):
+    questions = getAllQuestions()
+    print(questions)
+    print(questions[0])
+    for q in questions:
+        if q.id == id:
+            return q
+    return None
+
+def get_replies_by_question(q_id):
+    replies = getAllReplies()
+    result = []
+    print(replies)
+    print(replies[0])
+    #无故报错 说reply对象不可迭代？
+
+    for i in range(len(replies)):
+        r = replies[i]
+        if r.question_id == q_id:
+            result.append(r)
+    return result
+
+def get_pet_by_id(pet_id):
+    pets = Pets.query.filter_by(id=pet_id).all()
+    for p in pets:
+        if p.id == pet_id:
+            return p
+    return None
+
+def get_doctor_by_id(doctor_id):
+    doctors = Doctors.query.filter_by(id=doctor_id).all()
+    for d in doctors:
+        if d.id == doctor_id:
+            return d
+    return None
+
+def deleteAppointment(appointmentid):
+    Appointments.query.filter_by(id=appointmentid).delete()
+    db.session.commit()
+
+
+
+def databaseDebug():
+    #调试数据库
+    addUser("donlin", "000")
+    showUsers(getAllUser())
+
+    updateUserBirthday("donlin", "1998")
+    showUsers(getAllUser())
+
+    updateUserEmail("donlin", "donlin@qq.com")
+    showUsers(getAllUser())
+
+    updateUserGender("donlin", True)
+    showUsers(getAllUser())
+
+    print(checkUserExist("donlin"))
+    print(checkUserExist("donlin1"))
+
+    deleteUser("donlin")
+    showUsers(getAllUser())
+
+def checkHashLoop(hash):
+    pass_hash=generate_password_hash("hello")
+    print("hash is ")
+    print(pass_hash)
+    while True:
+        i=input()
+        print("check result:")
+        print(check_password_hash(pass_hash,i))
---
+++ Web/healingpaws/models.py
@@ -0,0 +1,86 @@
+from healingpaws import db
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    username=db.Column(db.String(64))
+    passwordHash=db.Column(db.String(64))
+    user_level=db.Column(db.Integer)
+    email=db.Column(db.String,default="no email")
+    dateOfBirth=db.Column(db.String,default="unknown birthday")
+    isMale=db.Column(db.Boolean,default=True)
+    pet=db.relationship('Pets',backref="pet_owner",lazy="dynamic")
+    questions = db.relationship('Question', backref="questions", lazy="dynamic")
+    replies = db.relationship('Reply',backref="replies", lazy="dynamic")
+    doctorInfo=db.relationship("Doctors",backref="doctor_user",lazy="dynamic")
+
+    def __repr__(self):
+        return "id : "+str(self.id)+" name : "+self.username+" email : "+self.email+" birthday : "+self.dateOfBirth+" male : "+str(self.isMale) +" level : "+str(self.user_level)+"\n"
+
+class Pets(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    petsname=db.Column(db.String(64))
+    owner=db.Column(db.Integer,db.ForeignKey('user.id'))
+    #这个health属性改为描述目前宠物的处理！ 只能员工操作！
+    #['Surgery date confirmed', 'Surgery Completed', 'Ready for Release', 'Wait for Surgery'] 0 1 2 3
+    health=db.Column(db.Integer,default=-1)
+    birthDay=db.Column(db.String,default="unknown birthday")
+    appoinment=db.relationship('Appointments',backref="appoint_pet",lazy="dynamic")
+    def __repr__(self):
+        return "id : "+str(self.id)+" pet name : "+self.petsname+" owner : "+str(self.owner)+" health : "+str(self.health)+" birthday : "+self.birthDay +"\n"
+
+
+class Doctors(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    userId=db.Column(db.Integer,db.ForeignKey("user.id"))
+    doctorname=db.Column(db.String(64))
+    age=db.Column(db.Integer)
+    telphone=db.Column(db.String)
+    introduction=db.Column(db.String)
+    appoinment=db.relationship('Appointments',backref="appoint_doctor",lazy="dynamic")
+    def formatCode(self,format):
+        return str(self.id)+format+self.doctorname+format+str(self.age)+format+self.telphone+format+self.introduction
+
+    def __repr__(self):
+        return "id : "+str(self.id)+" doctor name : "+self.doctorname+" age : "+str(self.age)+" telphone : "+self.telphone+" introduction : "+self.introduction +"\n"
+
+class Appointments(db.Model):
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    pet=db.Column(db.Integer,db.ForeignKey('pets.id'))
+    doctor=db.Column(db.Integer,db.ForeignKey('doctors.id'))
+    date=db.Column(db.String)
+    #分院的位置 1北京 2上海 3成都,目前默认是北京
+    loc=db.Column(db.Integer,default=1)
+    #Appointment的状态只有员工可以更改
+    #status = 0 已处理(默认) "unchecked"
+    #status = 1 未处理      "checked"
+    status=db.Column(db.Integer,default=0)
+    #Appointment具体安排的时间段 只有员工可以操作
+    time_slot = db.Column(db.String)
+    #紧急程度 1 urgent/ 0 normal
+    emergency = db.Column(db.Integer)
+    #对本次预约的描述
+    description = db.Column(db.String)
+
+    def __repr__(self):
+        return 'id: '+str(self.id)+' pet: '+str(self.pet)+' doctor: '+str(self.doctor)+' date: '+str(self.date)
+
+
+
+#一级提问主题
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String)
+    replys = db.relationship('Reply', backref="reolys",lazy="dynamic")
+
+    def __repr__(self):
+        return "id : "+str(self.id)+" user_id: "+ str(self.user) +" title: "+ self.title
+
+
+#二级评论,相当于两个用户在该问题下聊天
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.String)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    def __repr__(self):
+        return "id : "+str(self.id)+" user_id: "+ str(self.user) + " content: "+ self.content + "question_id" + str(self.question_id)
