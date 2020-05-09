e2850f1bd4d89373aaedd13db69296e9ee78e1e5
CDTD-LLT <ziqi.li@ucdconnect.ie>
2020-05-09 10:00:00 +0800

    bootstrap

--- Web/healingpaws/dbConnector.py
+++ Web/healingpaws/dbConnector.py
@@ -28,7 +28,7 @@ def addUser(username,password):
     add_question(u1.id)
     db.session.add(u1)
     db.session.commit()
-#sadasdwaodpkl,;adawdwdasdasdsadasdasdasdsdasdasdasdasdsasdadssdassdadsasdsaasdasdasdasdssadasdasdsa213
+#sadasdwaodpkl,;adawdwdasdasdsadasdasdasdsdasdasdasdasdsasdadssdassdadsasdsaasdasdasdasdssadasdasdsa213asdas
 def updateUserBirthday(username,birthday):
     if not birthday:
         return
